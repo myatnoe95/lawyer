3ada48a4c54aca36e5526ab46d0c7bd6f0787ba1
Myat Noe <khingsunthwe@gmail.com>
2020-09-18 15:56:43 +0630

    Meta tag and Law School login

--- src/components/LawSchoolBanner/LawSchoolBanner.js
+++ src/components/LawSchoolBanner/LawSchoolBanner.js
@@ -3,6 +3,8 @@ import {Jumbotron as Jumbo, Container , Row, Col, Form, Button} from 'react-boot
 import styled from 'styled-components';
 import * as Icon from 'react-bootstrap-icons';
 import LawSchoolimg from '../../assets/image/lawschool1.jpg';
+import LawSchoollogo from '../../assets/image/School of Law logo.jpg';
+import logo from '../../assets/icon/logo.jpg';
 
 
 const Styles =styled.div`
@@ -44,7 +46,8 @@ const Styles =styled.div`
       padding:60px;
       border-radius: 15px;
       box-shadow: 0px 0px 3px #777;
-      margin-top: 2%;
+      margin-top: 4%;
+      height: 300px;
    }
 
    .form{
@@ -66,13 +69,33 @@ const Styles =styled.div`
        text-align: center;
        color: #fff;
    }
+   .title{
+       margin-top: 15px;
+   }
+
+   .main-title{
+      
+   }
 
    ul{
        color: #fff;
        list-style-type: none;
        font-size: 16px;
    }
+   .singup{
+       margin-top: 35%;
+       color: #fff;
+   }
+   .singupbtn{
+     margin-top: 23%;
+     margin-right: 80px;
+   }
 
+   .schoollogo{
+       width: 150px;
+       height: 150px;
+    //    border-radius: 50%;
+   }
    @media(max-width:480px){
     .jumbo{
         height: auto;
@@ -84,7 +107,22 @@ const Styles =styled.div`
      }
      .login-form{
         margin-top: 15%;
+        height: auto;
      }
+     .schoollogo{
+        width: 100% !important;
+        height: 180px !important;
+    }
+    .loginbtn{
+        width: 100% !important;
+    }
+    .singupbtn{
+        margin-right:0px!important;
+        margin-top:15px!important;
+    }
+       
+    }
+
    }
 
   
@@ -121,27 +159,60 @@ export const LawSchoolBanner = (props)=>(
                         </ul>
                     </Col>
 
-                    <Col md={4} sm={12} xs={12}>
-                    
-                    </Col>
+                     <Col md={4} sm={12} xs={12}>
+                   
+                        {/* <h3 className="singup">Click Here to join Law School! </h3>
+                        <Icon.CaretDown size={30} color="#101010"/> 
+                        <a href="http://www.school.uphoephyu.org/login" style={{textDecoration:"none"}}> 
+                        <div className="singupbtn">
+                           <Button >Click Here to join Law School!</Button>  
+                        </div>
+                        </a> 
+                        */}
+                        
+                 
+                    </Col> 
+
+                   
+
+
 
                     <Col className="login-form"  md={4} sm={12} xs={12}>
-                            <Form  className="form">
-                            <h3>Law School Login</h3>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label className="label">Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                            </Form.Group>
+                       <h3 className=" text-center"><b>Law School Login Here!</b></h3>
+                        <Row className="title" >
+                            <Col md={6} sm={12} xs={12}>
+                              <img src={logo} className="schoollogo img-thumbnail"></img>
+                            </Col>
 
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label className="label">Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
+                            <Col md={6} sm={12} xs={12}>
+                               <a href="http://www.school.uphoephyu.org/login" style={{textDecoration:"none"}}> 
+                                <div className="singupbtn">
+                                <Button className="loginbtn">Login Here!</Button>  
+                                </div>
+                            </a> 
+                            </Col>
+
+                        </Row>
+                           
                         
-                            <Button variant="primary" type="submit">
-                                Submit
-                            </Button>
-                        </Form>
+                            {/* <Form  
+                               className="form"
+                            >
+                                <h3>Law School Login</h3>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label className="label">Email address</Form.Label> 
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label className="label">Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                            
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                        </Form> */}
                     
                     </Col>
                 </Row>
