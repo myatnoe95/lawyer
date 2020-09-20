d5af01dcdacb58561cc7001575a986619a715122
YeYintKo <yeyintko.mkn@gmail.com>
2020-09-21 04:12:39 +0630

    fixed server.js for 404 in production

--- server.js
+++ server.js
@@ -9,9 +9,27 @@ const fs = require('fs');
 app.use(express.static(path.resolve(__dirname, './build')));
 // create a GET route
 app.get('/express_backend', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  res.send({
+    express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'
   });
+});
 
 
-  
+
+app.get('*', (request, response) => {
+  console.log('Home page visited!');
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    // data = data.replace(/\$OG_TITLE/g, 'A Seamen Club');
+    // data = data.replace(/\$OG_DESCRIPTION/g, 'Job at Sea, Vacancies for Seafarer, Seamen Mobile App, Maritime Training School, Maritime News, Navigation, Marine Engineering, Seaman News, Seaman Club');
+    // result = data.replace(/\$OG_IMAGE/g, '');
+    response.send(result);
+  })
+});
+
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
